415b3ae440fd1fb157a5e0c0096aa8b1c51158dd
anappleguy <aronn12nz@gmail.com>
2024-08-03 15:30:37 +1200

    Update Aron - follow line.py
    This is the best code and is better than the code "jerry" wrote.............

--- Aron - follow line.py
+++ Aron - follow line.py
@@ -1,4 +1,4 @@
-from hub import light_matrix, port
+from hub import light_matrix, port # this adds ports
 import runloop, motor_pair, color_sensor, color
 motors = motor_pair #this shortens long code
 colour = color_sensor 
@@ -10,15 +10,15 @@ def followline(): # this is the function
     lgreen = colour.color(port.C) #finding the color the sensors are seeing
     rgreen = colour.color(port.D)
     if lgreen is color.GREEN or rgreen is color.GREEN:# this move me left or right
-        if lgreen is color.GREEN:
-            motors.move_for_degrees(motors.PAIR_1, 360, -45, velocity = 90)
-        if rgreen is color.GREEN:
-            motors.move_for_degrees(motors.PAIR_1, 360, 45, velocity = 90)
+        if lgreen is color.GREEN: # if the left sensor discovers green then...
+            motors.move_for_degrees(motors.PAIR_1, 360, -45, velocity = 90) # the robot turns left
+        if rgreen is color.GREEN: # if the right sensor discovers green then...
+            motors.move_for_degrees(motors.PAIR_1, 360, 45, velocity = 90) # the robot turns right
 
-    else:
+    else: # if there are no green squares
         lcolour = colour.reflection(port.C) #this finds the reflected colour of the sensors
         rcolour = colour.reflection(port.D)
-        direction = (lcolour - rcolour) # this calculates the angle to go
+        direction = (lcolour - rcolour) # this calculates the angle to go forward
         motors.move(motors.PAIR_1, direction, velocity = 360) #this moves the robot along the line
 
 while True:
