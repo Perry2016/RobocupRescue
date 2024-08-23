d5830416a6dfb4a01ad6d98571a66b17fefbb114
Jerry4-0-4 <ohyes.ve@gmail.com>
2024-08-23 16:59:17 +1200

    Update doublesensor_Jerry.py

--- doublesensor_Jerry.py
+++ doublesensor_Jerry.py
@@ -1,42 +1,43 @@
-from motor import stop
+
 import motor_pair
 import color_sensor
 from hub import port
 import runloop
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+
 
 
 async def main():
-    
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
-    
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 50, 50)
-
     while True:
-        reflection_rateA = color_sensor.reflection(port.E)
-        reflection_rateB = color_sensor.reflection(port.F)
+        reflection_rateE = color_sensor.reflection(port.E)
+        reflection_rateF = color_sensor.reflection(port.F)   
+         
+        print(reflection_rateE)
+
+        # turn left
+        if reflection_rateE <= 35:
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 100, 0, 1000)
+            print("turn left")
+
+        # turn right
+        if reflection_rateF <= 35:
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 100, 1000, 0)
+            print("turn right")
+
+        # left color sensor detecting green
+        if reflection_rateE > 45 and reflection_rateE <= 70:
+
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 0, 1000)
+            print("left sensor detected green")
+
+        # right color sensor detecting green
+        if reflection_rateF > 45 and reflection_rateF <= 70:
+            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 1000, 0)
+            print("left sensor detected green")
 
         
-        if reflection_rateA <= 35:
-
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 400, 0)
-
-        if reflection_rateB <= 35:
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 0, 400)
-
-        if reflection_rateA <= 70 and reflection_rateA > 50:
-
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 0, 400)
-
-        if reflection_rateB <= 70 and reflection_rateB > 50:
-            motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 400, 0)
-        
-        if reflection_rateA >= 55 and reflection_rateB >= 55:
-            
+        if reflection_rateE >= 55 and reflection_rateF >= 55:
             motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 50, 100, 100)
+            print("go straight")
 
-        
-    
-        
-
-        
 runloop.run(main())
